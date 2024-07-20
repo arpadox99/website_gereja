7c9039ba921c46fa03435c21da01f369b4466d45
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-07-21 01:14:34 +0900

    21 JULY UPDATE - ADMIN MENU

--- admin/mediaadd.php
+++ admin/mediaadd.php
@@ -7,7 +7,7 @@
   </div>
   <div class="row justify-content-center">
     <div class="col-lg-7">
-      <div class="card shadow-lg border-0 rounded-lg mt-5">
+      <div class="card shadow-lg border-1 rounded-lg mt-5">
         <div class="card-header text-bg-info">
           <h3 class="text-center fw-bold my-3"> INPUT DATA GAMBAR </h3>
         </div>
